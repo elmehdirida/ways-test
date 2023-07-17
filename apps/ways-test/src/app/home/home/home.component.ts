ad9c767f114294bbf369f4c0eaeda2eee34bb5f9
mohsine1999 <mohsine090hajjar@gmail.com>
2023-07-17 17:12:26 +0100

    ojfghjô

--- apps/ways-test/src/app/home/home/home.component.ts
+++ apps/ways-test/src/app/home/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardLetterComponent, UiModule } from '@ways-test/ui';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'ways-test-home',
   standalone: true,
-  imports: [CommonModule,UiModule,CardLetterComponent],
+  imports: [CommonModule,UiModule,CardLetterComponent,MatButtonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
