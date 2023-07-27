7d80e3308971c4130e5a810cb97ff283b7d9adb9
elmehdirida <elmehdi.fst@uhp.ac.ma>
2023-07-27 10:46:41 +0100

    from change to input

--- apps/ways-test/src/app/app.component.ts
+++ apps/ways-test/src/app/app.component.ts
@@ -4,18 +4,36 @@ import {PrincipalComponent} from "./principal/principal.component";
 import { MatNativeDateModule } from '@angular/material/core';
 import { DataAccessModule, LetterService } from '@ways-test/data-access';
 import { HomeComponent } from './home/home/home.component';
+import {NgClass, NgForOf} from "@angular/common";
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule} from "@angular/forms";
 
 @Component({
     standalone: true,
     selector: 'ways-test-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [RouterModule, PrincipalComponent, MatNativeDateModule, DataAccessModule, HomeComponent]
+  imports: [RouterModule, PrincipalComponent, MatNativeDateModule, DataAccessModule, HomeComponent, NgClass, MatSelectModule, NgForOf, FormsModule]
 })
 export class AppComponent  {
   constructor() {}
 
-  title = 'ways-test';
+  title = 'ways-test'
+  currentTheme =''
+  themes = [
+    {
+      label: 'dark',
+      value: 'dark-theme',
+    },
+    {
+      label: 'blue-light',
+      value: 'blue-light-theme',
+    },
+    {
+      label: 'default',
+      value: '',
+    }
+  ]
 
 
 }
