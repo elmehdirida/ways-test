61dacd220dd03c3c9d8f499600ba958c2b43348b
mohsine1999 <mohsine090hajjar@gmail.com>
2023-07-27 12:32:01 +0100

    here we go

--- apps/ways-test/src/app/app.component.ts
+++ apps/ways-test/src/app/app.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {PrincipalComponent} from "./principal/principal.component";
+import { PrincipalComponent } from './principal/principal.component';
 import { MatNativeDateModule } from '@angular/material/core';
 import { DataAccessModule, LetterService } from '@ways-test/data-access';
 import { HomeComponent } from './home/home/home.component';
-import {NgClass, NgForOf} from "@angular/common";
-import {MatSelectModule} from "@angular/material/select";
-import {FormsModule} from "@angular/forms";
+import { NgClass, NgForOf } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-    standalone: true,
-    selector: 'ways-test-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-  imports: [RouterModule, PrincipalComponent, MatNativeDateModule, DataAccessModule, HomeComponent, NgClass, MatSelectModule, NgForOf, FormsModule]
+  standalone: true,
+  selector: 'ways-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    RouterModule,
+    PrincipalComponent,
+    MatNativeDateModule,
+    DataAccessModule,
+    HomeComponent,
+    NgClass,
+    MatSelectModule,
+    NgForOf,
+    FormsModule,
+  ],
 })
-export class AppComponent  {
+export class AppComponent {
   constructor() {}
 
-  title = 'ways-test'
-  currentTheme =''
+  title = 'ways-test';
+  currentTheme = '';
   themes = [
     {
       label: 'dark',
@@ -32,8 +42,6 @@ export class AppComponent  {
     {
       label: 'default',
       value: '',
-    }
-  ]
-
-
+    },
+  ];
 }
