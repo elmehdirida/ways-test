5353a7ddb58b7149cf9bcf7e8577802b317bd833
elmehdirida <elmehdi.fst@uhp.ac.ma>
2023-08-02 16:20:00 +0100

    storybook design

--- libs/ui/src/lib/nativeSelect/native-select.component.spec.ts
+++ libs/ui/src/lib/nativeSelect/native-select.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NativeSelectComponent } from './native-select.component';
+import { NativeSelectComponent } from '@ways-test/ui';
 
 describe('NativeSelectComponent', () => {
   let component: NativeSelectComponent;
