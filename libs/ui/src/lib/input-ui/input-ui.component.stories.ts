699f143e8122f7ba10354ffce9de5b8970d3baa0
mohsine1999 <mohsine090hajjar@gmail.com>
2023-08-24 11:05:02 +0100

    handleSelectedValuesWhenAddingOptionFromSearch

--- libs/ui/src/lib/input-ui/input-ui.component.stories.ts
+++ libs/ui/src/lib/input-ui/input-ui.component.stories.ts
@@ -1,17 +1,16 @@
-import {applicationConfig, Meta, moduleMetadata} from '@storybook/angular';
+import { applicationConfig, Meta, moduleMetadata } from '@storybook/angular';
 import { InputUiComponent } from './input-ui.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import {importProvidersFrom} from "@angular/core";
-import {HttpClientModule} from "@angular/common/http";
+import { importProvidersFrom } from '@angular/core';
 
 export default {
   title: 'Inputs/InputUiComponent',
   component: InputUiComponent,
   argTypes: {
-    onChangeValue : {action : "changed value"},
-    placeHolder: { control: 'text'  },
+    onChangeValue: { action: 'changed value' },
+    placeHolder: { control: 'text' },
     value: { control: 'text' },
     required: {
       control: 'boolean',
@@ -31,7 +30,6 @@ export default {
     applicationConfig({
       providers: [importProvidersFrom(BrowserAnimationsModule)],
     }),
-
   ],
 } as Meta<InputUiComponent>;
 
@@ -44,5 +42,29 @@ export const Primary = {
     value: 'input value',
     required: false,
     type: 'text',
+  },
+};
+
+export const Editable = {
+  render: (args: InputUiComponent) => ({
+    props: args,
+  }),
+  args: {
+    placeHolder: 'Label',
+    value: 'Lorem ipsum dolor sit amet.',
+    type: 'text',
+    disable: false,
+  },
+};
+
+export const ReadOnly = {
+  render: (args: InputUiComponent) => ({
+    props: args,
+  }),
+  args: {
+    placeHolder: 'Label',
+    value: 'Lorem ipsum dolor sit amet.',
+    type: 'text',
+    disable: true,
   },
 };
